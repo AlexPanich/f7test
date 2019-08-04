6c72f9d3f619df176a318605a743b063311b1bca
Александр Паниченко <aleksandrpanicenko@MacBook-Pro-Aleksandr.local>
2019-08-04 22:42:35 +0300

    3

--- build/precache-manifest.a07d17c8450427ccd004d538f1cb8619.js
+++ build/precache-manifest.a07d17c8450427ccd004d538f1cb8619.js
@@ -8,12 +8,12 @@ self.__precacheManifest = [
     "url": "/f7test/static/js/main.4e3b5365.chunk.js"
   },
   {
-    "revision": "efbf8fe642b64b321069",
+    "revision": "e5b1c37e0b49debfc8b7",
     "url": "/f7test/static/css/1.d0d578fd.chunk.css"
   },
   {
-    "revision": "efbf8fe642b64b321069",
-    "url": "/f7test/static/js/1.efbf8fe6.chunk.js"
+    "revision": "e5b1c37e0b49debfc8b7",
+    "url": "/f7test/static/js/1.e5b1c37e.chunk.js"
   },
   {
     "revision": "0c66712cd63effe85951",
@@ -28,6 +28,14 @@ self.__precacheManifest = [
     "url": "/f7test/static/media/MaterialIcons-Regular.012cf6a1.woff"
   },
   {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/f7test/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "28db829912bb402df5b1e6fb16f1908c",
+    "url": "/f7test/static/media/Framework7Icons-Regular.28db8299.eot"
+  },
+  {
     "revision": "19158ff09eb907432a7ccacd074ef9d5",
     "url": "/f7test/static/media/Framework7Icons-Regular.19158ff0.woff2"
   },
@@ -40,19 +48,11 @@ self.__precacheManifest = [
     "url": "/f7test/static/media/MaterialIcons-Regular.e79bfd88.eot"
   },
   {
-    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
-    "url": "/f7test/static/media/MaterialIcons-Regular.a37b0c01.ttf"
-  },
-  {
-    "revision": "28db829912bb402df5b1e6fb16f1908c",
-    "url": "/f7test/static/media/Framework7Icons-Regular.28db8299.eot"
-  },
-  {
     "revision": "111371010ef70bbcf7e6968abc8e2fb2",
     "url": "/f7test/static/media/Framework7Icons-Regular.11137101.ttf"
   },
   {
-    "revision": "ad25e545d2c142c325280254d2213c7c",
+    "revision": "3c054faf5c3a346dccefb69b584a22a2",
     "url": "/f7test/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9886544660a3aecf83a090c26fcb1f0.js"
+  "/f7test/precache-manifest.a07d17c8450427ccd004d538f1cb8619.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/f7test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
