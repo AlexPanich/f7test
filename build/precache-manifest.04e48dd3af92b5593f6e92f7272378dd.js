5d560d64d6da7cdb62ba81c0f5c7af39500059c4
Александр Паниченко <aleksandrpanicenko@MacBook-Pro-Aleksandr.local>
2019-08-04 22:36:17 +0300

    2

---
+++ build/precache-manifest.04e48dd3af92b5593f6e92f7272378dd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e3b5365154bc5a25594",
+    "url": "/f7test/static/css/main.bb145c7d.chunk.css"
+  },
+  {
+    "revision": "4e3b5365154bc5a25594",
+    "url": "/f7test/static/js/main.4e3b5365.chunk.js"
+  },
+  {
+    "revision": "efbf8fe642b64b321069",
+    "url": "/f7test/static/css/1.d0d578fd.chunk.css"
+  },
+  {
+    "revision": "efbf8fe642b64b321069",
+    "url": "/f7test/static/js/1.efbf8fe6.chunk.js"
+  },
+  {
+    "revision": "0c66712cd63effe85951",
+    "url": "/f7test/static/js/runtime~main.0c66712c.js"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/f7test/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/f7test/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "19158ff09eb907432a7ccacd074ef9d5",
+    "url": "/f7test/static/media/Framework7Icons-Regular.19158ff0.woff2"
+  },
+  {
+    "revision": "5fce7c1238bad823bee064bd127fe4cd",
+    "url": "/f7test/static/media/Framework7Icons-Regular.5fce7c12.woff"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/f7test/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/f7test/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "28db829912bb402df5b1e6fb16f1908c",
+    "url": "/f7test/static/media/Framework7Icons-Regular.28db8299.eot"
+  },
+  {
+    "revision": "111371010ef70bbcf7e6968abc8e2fb2",
+    "url": "/f7test/static/media/Framework7Icons-Regular.11137101.ttf"
+  },
+  {
+    "revision": "ad25e545d2c142c325280254d2213c7c",
+    "url": "/f7test/index.html"
+  }
+];
